5005834e2e876b2b2c5baec2311d52a18f54cb8e
georggreat <georggreat@mail.ru>
2017-09-27 18:34:21 +0300

    first commit

---
+++ Aletdinov_lab3_task1/Aletdinov_task3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Aletdinov_task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButtonDraw = new System.Windows.Forms.RadioButton();
+            this.radioButtonFill = new System.Windows.Forms.RadioButton();
+            this.radioButtonImage = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(768, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButtonDraw
+            // 
+            this.radioButtonDraw.AutoSize = true;
+            this.radioButtonDraw.Checked = true;
+            this.radioButtonDraw.Location = new System.Drawing.Point(207, 13);
+            this.radioButtonDraw.Name = "radioButtonDraw";
+            this.radioButtonDraw.Size = new System.Drawing.Size(51, 17);
+            this.radioButtonDraw.TabIndex = 1;
+            this.radioButtonDraw.TabStop = true;
+            this.radioButtonDraw.Text = "Перо";
+            this.radioButtonDraw.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFill
+            // 
+            this.radioButtonFill.AutoSize = true;
+            this.radioButtonFill.Location = new System.Drawing.Point(264, 13);
+            this.radioButtonFill.Name = "radioButtonFill";
+            this.radioButtonFill.Size = new System.Drawing.Size(68, 17);
+            this.radioButtonFill.TabIndex = 1;
+            this.radioButtonFill.Text = "Заливка";
+            this.radioButtonFill.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonImage
+            // 
+            this.radioButtonImage.AutoSize = true;
+            this.radioButtonImage.Enabled = false;
+            this.radioButtonImage.Location = new System.Drawing.Point(338, 13);
+            this.radioButtonImage.Name = "radioButtonImage";
+            this.radioButtonImage.Size = new System.Drawing.Size(95, 17);
+            this.radioButtonImage.TabIndex = 1;
+            this.radioButtonImage.Text = "Изображение";
+            this.radioButtonImage.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 582);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButtonImage);
+            this.Controls.Add(this.radioButtonFill);
+            this.Controls.Add(this.radioButtonDraw);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Заливное от Фадиса";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButtonDraw;
+        private System.Windows.Forms.RadioButton radioButtonFill;
+        private System.Windows.Forms.RadioButton radioButtonImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Aletdinov_lab3_task1/Aletdinov_task3/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aletdinov_task3
+{
+    
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap bmpImage;
+        Pen myPen, myFillPen;
+        int myPenThickness;
+        Color myPenColor;
+        Color oldColor;
+        Color myFillColor;
+        Point curPoint, prevPoint;
+        bool isPressed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            myPenThickness = 2;
+            myPenColor = Color.Red;
+            myFillColor = Color.Yellow;
+            myPen = new Pen(myPenColor, myPenThickness);
+            myFillPen = new Pen(myFillColor, 1);
+            isPressed = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                prevPoint = curPoint;
+                curPoint = e.Location;
+                Graphics g = Graphics.FromImage(bmp);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; 
+
+                g.DrawLine(myPen, curPoint, prevPoint);
+                
+                g.Dispose();
+                pictureBox1.Image = bmp;
+                pictureBox1.Invalidate();
+   
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isPressed = false;
+            prevPoint = curPoint;
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                isPressed = true;
+            curPoint = e.Location;
+            
+            {
+                oldColor = bmp.GetPixel(curPoint.X, curPoint.Y);
+                if (oldColor.ToArgb()!=myFillColor.ToArgb())
+                if (radioButtonFill.Checked)
+                {
+                    fill(curPoint, oldColor, myFillColor);
+                }
+                if (radioButtonImage.Checked)
+                    fillImage(curPoint);
+            }
+        }
+
+        private void fill(Point startPoint, Color oldColor, Color newColor)
+        {
+            if(bmp.GetPixel(startPoint.X,startPoint.Y)==oldColor)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+
+                var left = find_left(startPoint, oldColor);
+                var right = find_right(startPoint, oldColor);
+                g.DrawLine(myFillPen, left, right);
+                g.Dispose();
+                pictureBox1.Image = bmp;
+                pictureBox1.Invalidate();
+
+                for (var x = left.X; x < right.X; x++)
+                {
+                    if (startPoint.Y > 1)
+                        fill(new Point(x, startPoint.Y - 1), oldColor, newColor);
+                    else break;
+                }
+                for (var x = left.X; x < right.X; x++)
+                {
+                    if (startPoint.Y < bmp.Height - 1)
+                        fill(new Point(x, startPoint.Y + 1), oldColor, newColor);
+                    else break;
+                }
+            }            
+        }
+
+        private void fillImage(Point startPoint)
+        {
+            if (bmp.GetPixel(startPoint.X, startPoint.Y) == oldColor)
+            {
+                Graphics g = Graphics.FromImage(bmp);
+                var left = find_left(startPoint, oldColor);
+                var right = find_right(startPoint, oldColor);
+
+                for (var x = left.X; x < right.X; x++)
+                {
+                    int xW = x % bmpImage.Width;
+                    int yH = startPoint.Y % bmpImage.Height;
+                    Color c = bmpImage.GetPixel(xW, yH);
+                    bmp.SetPixel(x, startPoint.Y, c);
+                }   
+                
+                
+                g.Dispose();
+                pictureBox1.Image = bmp;
+                pictureBox1.Invalidate();
+
+                for (var x = left.X; x < right.X; x++)
+                {
+                    if (startPoint.Y > 1)
+                        fillImage(new Point(x, startPoint.Y - 1));
+                    else break;
+                }
+                for (var x = left.X; x < right.X; x++)
+                {
+                    if (startPoint.Y < bmp.Height - 1)
+                        fillImage(new Point(x, startPoint.Y + 1));
+                    else break;
+                }
+                
+            }
+
+        }
+        private Point find_left(Point startPoint, Color oldColor)
+        {
+            int x=startPoint.X;
+            while((x>1) && (bmp.GetPixel(x, startPoint.Y) == oldColor))
+            {
+                x--;
+            }
+            return new Point(x+1, startPoint.Y);
+        }
+        private Point find_right(Point startPoint, Color oldColor)
+        {
+            int x = startPoint.X;
+            while ((x < bmp.Width) && (bmp.GetPixel(x, startPoint.Y) == oldColor))
+            {
+                x++;
+            }
+            return new Point(x-1, startPoint.Y);
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                try
+                {
+                    bmpImage = new Bitmap(openFileDialog1.FileName);
+                    radioButtonImage.Enabled = true;
+                }
+                catch
+                {
+                    radioButtonImage.Enabled = false;
+                    MessageBox.Show("Не могу открыть изображение.");
+                }
+                return;
+            }
+        }
+    }
+}
